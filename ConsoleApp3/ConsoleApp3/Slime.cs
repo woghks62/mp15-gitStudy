87fe1d3d50de3db4bd904d6f26440e1aa1168437
woghks62 <p2pee@naver.com>
2026-08-27 11:06:35 +0900

    [Feat] 슬라임 클래스 생성

---
+++ ConsoleApp3/ConsoleApp3/Slime.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApp3;
+
+public class Slime
+{
+    public string Name;
+    public int HP;
+
+    public void TakeDamage(int damage)
+    {
+    }
+
+    public void Attack()
+    {
+        
+    }
+}
