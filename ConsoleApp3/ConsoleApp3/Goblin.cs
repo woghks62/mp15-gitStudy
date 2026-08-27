edacee87e89e7ce3a3cb959a55c2fda6f6375343
woghks62 <p2pee@naver.com>
2026-08-27 11:07:36 +0900

    [Feat] 고블린 클래스 생성

---
+++ ConsoleApp3/ConsoleApp3/Goblin.cs
@@ -0,0 +1,19 @@
+namespace ConsoleApp3;
+
+public class Goblin
+{
+    public string Name;  
+    public int Hp;
+
+    public void TakeDamage(int damage)
+    {
+        /* ... */ 
+        
+    }
+
+    public void Attack()
+    {
+        /* 단검을 휘두릅니다 */ 
+        
+    }  
+}
